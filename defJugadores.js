c6e455e0a93ca636259e25200c1e2126e98aebf4
Antonio Gennaro <antgennaro@hotmail.com>
2023-10-23 15:22:22 -0300

    POO ejercicio Polimorfismo con Gemas. v1

--- defJugadores.js
+++ defJugadores.js
@@ -6,7 +6,15 @@ const jugador1 = [];
 const jugador2 = [];
 const jugadores = [jugador1, jugador2 ];
 //
-console.log("Defino 2 jugadores con un nro aleatorio de gemas c/u");
+console.log("----------------------------------------------");
+console.log("*** JUEGO DE LAS GEMAS DEL UNIVERSO MARVEL ***");
+console.log("----------------------------------------------");
+console.log("Se definen 2 jugadores con un nro aleatorio de gemas c/u");
+console.log(" pueden tener de 1 a 6 c/u (con la condicón que no se repitan)");
+console.log("Se describen las Gemas que tiene c/u y se determina quién gana");
+console.log(" en función de la cantidad y si hay un empate, se desempta por");
+console.log(" si alguno de ellos tiene la de Alma y/o Poder. Aun así podría");
+console.log(" haber un empate virtual... ver los detalles...");
 //
 asignaGemas (jugador1);
 asignaGemas (jugador2);
